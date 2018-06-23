c27af12f75f2df7b658fcf45cd6cc4f6a9fd99d1
DaveRevay2 <daverevay@gmail.com>
2018-06-23 12:52:50 +1000

    Add gpio writes

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const Gpio = require('onoff').Gpio;
 const app = express()
+const relayGroup1 = new Gpio(2, 'out');
+const relayGroup1 = new Gpio(3, 'out');
 
-app.get('/', (req, res) => {
-  console.log('here!')
-  res.send('Hello World')
+app.get('/speakers/down', (req, res) => {
+  relayGroup1.write(Gpio.LOW)
+  relayGroup2.write(Gpio.HIGH)
+  res.send('down')
+})
+
+app.get('/speakers/up', (req, res) => {
+  relayGroup1.write(Gpio.HIGH)
+  relayGroup2.write(Gpio.LOW)
+  res.send('up')
+})
+
+app.get('/speakers/both', (req, res) => {
+  relayGroup1.write(Gpio.LOW)
+  relayGroup2.write(Gpio.LOW)
+  res.send('both')
 })
 
 app.listen(3000)
